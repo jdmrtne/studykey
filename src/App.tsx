363e492eab919aa8d04e579920ad67e7cc88e6fc
jdmrtne <judemrtne@gmail.com>
2026-09-22 11:21:03 +0800

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "./hooks/useTheme";
+import { AppShell } from "./components/layout/AppShell";
+import { Dashboard } from "./pages/Dashboard";
+import { Lessons } from "./pages/Lessons";
+import { Quiz } from "./pages/Quiz";
+import { Flashcards } from "./pages/Flashcards";
+import { Reviewer } from "./pages/Reviewer";
+import { Chat } from "./pages/Chat";
+import { AISettings } from "./pages/AISettings";
+import { Settings } from "./pages/Settings";
+
+export default function App() {
+  return (
+    <ThemeProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppShell />}>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/lessons" element={<Lessons />} />
+            <Route path="/quiz" element={<Quiz />} />
+            <Route path="/flashcards" element={<Flashcards />} />
+            <Route path="/reviewer" element={<Reviewer />} />
+            <Route path="/chat" element={<Chat />} />
+            <Route path="/ai-settings" element={<AISettings />} />
+            <Route path="/settings" element={<Settings />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
